bea13da73ffa3e7a3810b3345788062d9c51832a
NachoConort <nachoconort@gmail.com>
2024-06-19 21:29:41 -0300

    primer modelo frontend login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import LoginForm from './loginForm';
+import '../styles/login.css'
+
 function Login() {
     return(
         <div>
-            Login route
+            <div>
+                <header>
+                    <div>
+                        <span>Sale fulbo</span>
+                    </div>
+                    <div>
+                        <h1>Alquiler de canchas en Tucumán</h1>
+                    </div>
+                    <div>
+                        <Link to="/">Volver</Link>
+                    </div>
+                </header>
+            </div>
+            <div className='container'>
+                <LoginForm />
+            </div>
         </div>
     )
 }
